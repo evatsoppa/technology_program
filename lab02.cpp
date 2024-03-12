a7e8f35f0bbad530920476eec9d1299f42db10e1
evatsoppa <145338773+evatsoppa@users.noreply.github.com>
2024-03-12 12:57:17 +0300

    Update lab02.cpp

--- lab02.cpp
+++ lab02.cpp
@@ -1,163 +1,88 @@
 #include <iostream>
-class vect {
+
+// Класс для векторов
+class Vector {
 public:
-    int dim;
-    double* b;
-    int num;
-    static int count;
-    vect(int d, double* arr) : dim(d), b(arr), num(++count) {
-        std::cout << "Created vector " << num << std::endl;
-    }
-    ~vect() {
-        std::cout << "Destroyed vector " << num << std::endl;
-    }
-    vect operator+(const vect& other) const {
-        double* result = new double[dim];
-        for (int i = 0; i < dim; ++i) {
-            result[i] = b[i] + other.b[i];
-        }
-        return vect(dim, result);
-    }
-    vect operator-(const vect& other) const {
-        double* result = new double[dim];
-        for (int i = 0; i < dim; ++i) {
-            result[i] = b[i] - other.b[i];
-        }
-        return vect(dim, result);
+    int dim;          // Размерность вектора
+    double* elements;  // Массив элементов вектора
+    int num;          // Номер вектора
+    static int count;  // Счетчик созданных векторов
+
+    // Конструктор создает вектор
+    Vector(int dimension, double* array) : dim(dimension), elements(array), num(++count) {
+        std::cout << "Создан вектор " << num << std::endl;
     }
 
-    vect operator-() const {
-        double* result = new double[dim];
-        for (int i = 0; i < dim; ++i) {
-            result[i] = -b[i];
-        }
-        return vect(dim, result);
+    // Деструктор уничтожает вектор
+    ~Vector() {
+        std::cout << "Уничтожен вектор " << num << std::endl;
     }
-    double operator*(const vect& other) const {
-        double result = 0.0;
-        for (int i = 0; i < dim; ++i) {
-            result += b[i] * other.b[i];
-        }
-        return result;
-    }
-    vect operator*(double k) const {
-        double* result = new double[dim];
-        for (int i = 0; i < dim; ++i) {
-            result[i] = k * b[i];
-        }
-        return vect(dim, result);
-    }
-    vect& operator=(const vect& other) {
-        if (this != &other) {
-            delete[] b;
-            dim = other.dim;
-            b = new double[dim];
-            for (int i = 0; i < dim; ++i) {
-                b[i] = other.b[i];
-            }
-        }
-        return *this;
-    }
+
+    // Операторы векторной алгебры
+    Vector operator+(const Vector& other) const;
+    Vector operator-(const Vector& other) const;
+    Vector operator-() const;
+    double operator*(const Vector& other) const;
+    Vector operator*(double scalar) const;
+    Vector& operator=(const Vector& other);
 };
-int vect::count = 0;
-class matr {
+
+// Статическая переменная инициализируется вне класса
+int Vector::count = 0;
+
+// Класс для матриц
+class Matrix {
 public:
-    int dim;
-    double* a;
-    int num;
-    static int count;
-    matr(int d, double* arr) : dim(d), a(arr), num(++count) {
-        std::cout << "Created matrix " << num << std::endl;
+    int dim;          // Размерность матрицы
+    double* elements;  // Массив элементов матрицы
+    int num;          // Номер матрицы
+    static int count;  // Счетчик созданных матриц
+
+    // Конструктор создает матрицу
+    Matrix(int dimension, double* array) : dim(dimension), elements(array), num(++count) {
+        std::cout << "Создана матрица " << num << std::endl;
     }
-    ~matr() {
-        std::cout << "Destroyed matrix " << num << std::endl;
+
+    // Деструктор уничтожает матрицу
+    ~Matrix() {
+        std::cout << "Уничтожена матрица " << num << std::endl;
     }
-    matr operator+(const matr& other) const {
-        double* result = new double[dim];
-        for (int i = 0; i < dim; ++i) {
-            result[i] = a[i] + other.a[i];
-        }
-        return matr(dim, result);
-    }
-    matr operator-(const matr& other) const {
-        double* result = new double[dim];
-        for (int i = 0; i < dim; ++i) {
-            result[i] = a[i] - other.a[i];
-        }
-        return matr(dim, result);
-    }
-    matr operator-() const {
-        double* result = new double[dim];
-        for (int i = 0; i < dim; ++i) {
-            result[i] = -a[i];
-        }
-        return matr(dim, result);
-    }
-    matr operator*(const matr& other) const {
-        double* result = new double[dim];
-        for (int i = 0; i < dim; ++i) {
-            result[i] = a[i] * other.a[i];
-        }
-        return matr(dim, result);
-    }
-    matr operator*(double k) const {
-        double* result = new double[dim];
-        for (int i = 0; i < dim; ++i) {
-            result[i] = k * a[i];
-        }
-        return matr(dim, result);
-    }
-    matr& operator=(const matr& other) {
-        if (this != &other) {
-            delete[] a;
-            dim = other.dim;
-            a = new double[dim];
-            for (int i = 0; i < dim; ++i) {
-                a[i] = other.a[i];
-            }
-        }
-        return *this;
-    }
-    vect operator*(const vect& v) const {
-        double* result = new double[dim];
-        for (int i = 0; i < dim; ++i) {
-            result[i] = 0.0;
-            for (int j = 0; j < dim; ++j) {
-                result[i] += a[i * dim + j] * v.b[j];
-            }
-        }
-        return vect(dim, result);
-    }
+
+    // Операторы векторной алгебры для матрицы
+    Matrix operator+(const Matrix& other) const;
+    Matrix operator-(const Matrix& other) const;
+    Matrix operator-() const;
+    Matrix operator*(const Matrix& other) const;
+    Matrix operator*(double scalar) const;
+    Matrix& operator=(const Matrix& other);
+
+    // Оператор умножения матрицы на вектор
+    Vector operator*(const Vector& vector) const;
 };
-int matr::count = 0;
+
+// Статическая переменная инициализируется вне класса
+int Matrix::count = 0;
+
+// Главная функция, где происходит работа с векторами и матрицами
 int main() {
+    // Создаем векторы
     double arr1[] = {1.0, 2.0, 3.0};
     double arr2[] = {4.0, 5.0, 6.0};
+    Vector vector1(3, arr1);
+    Vector vector2(3, arr2);
+
+    // Создаем матрицы
     double arr3[] = {7.0, 8.0, 9.0};
-    vect v1(3, arr1);
-    vect v2(3, arr2);
-    matr m1(3, arr1);
-    matr m2(3, arr2);
-    vect result1 = v1 + v2;
-    vect result2 = v1 - v2;
-    vect result3 = -v1;
-    double result4 = v1 * v2;
-    vect result5 = 2.0 * v1;
-    v1 = v2;
-    matr result6 = m1 + m2;
-    matr result7 = m1 - m2;
-    matr result8 = -m1;
-    matr result9 = m1 * m2;
-    matr result10 = 2.0 * m1;
-    m1 = m2;
-    vect result11 = m1 * v1;
-    delete[] result1.b;
-    delete[] result2.b;
-    delete[] result3.b;
-    delete[] result6.a;
-    delete[] result7.a;
-    delete[] result8.a;
-    delete[] result9.a;
-    delete[] result10.a;
+    Matrix matrix1(3, arr1);
+    Matrix matrix2(3, arr3);
+
+    // Выполняем операции векторной алгебры
+    Vector resultVector = vector1 + vector2;
+    Matrix resultMatrix = matrix1 + matrix2;
+
+    // Освобождаем память
+    delete[] resultVector.elements;
+    delete[] resultMatrix.elements;
+
     return 0;
 }
